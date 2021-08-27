ba1c1e511f5a815bc4d9dce84983e5f8025a4119
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-08-27 16:33:41 -0700

    Fix: expression evaluation

--- Calculator.js
+++ Calculator.js
@@ -1,59 +1,57 @@
 class Calculator {
     constructor(expression) {
         this.expression = expression;
-        this.arrayExpression = [];
         this.openParentheses = 0;
     }
 
     evaluateExpression() {
         const arrayExpression = this.parseExpression();
 
-        if(arrayExpression.length === 0) return 0;
+        if (arrayExpression.length === 0) return 0;
 
         const numbers = [];
         const operations = [];
 
-        for(let i = 0; i < arrayExpression.length; i++){
+        for (let i = 0; i < arrayExpression.length; i++) {
 
             if (!isNaN(arrayExpression[i])) {
                 numbers.push(arrayExpression[i]);
-                
-            } else if (arrayExpression[i] === '('){
+
+            } else if (arrayExpression[i] === '(') {
                 operations.push('(');
 
-            } else if (arrayExpression[i] === ')'){
+            } else if (arrayExpression[i] === ')') {
 
-                while(operations[operations.length - 1] !== '(') {
-                    
-                    let operator = operations.pop();
-                    numbers.push(this.calculate(numbers.pop(), operator, numbers.pop()))
-                    
+                while (operations[operations.length - 1] !== '(') {
+
+                    numbers.push(this.calculate(numbers.pop(), operations.pop(), numbers.pop()))
+
                 }
                 operations.pop();
 
             } else {
-                while(operations.length > 0 && this.checkPrecedence(arrayExpression[i], operations[operations.length - 1])){
+                while (operations.length > 0 && this.checkPrecedence(arrayExpression[i], operations[operations.length - 1])) {
                     numbers.push(this.calculate(numbers.pop(), operations.pop(), numbers.pop()))
                 }
                 operations.push(arrayExpression[i]);
-                
+
             }
         }
 
-        while(operations.length !== 0){
+        while (operations.length !== 0) {
             numbers.push(this.calculate(numbers.pop(), operations.pop(), numbers.pop()))
         }
         return numbers.pop();
     }
 
-    checkPrecedence(operator1, operator2){
-        if(operator2 === '(' || operator2 === ')') return false;
-        if((operator1 === '*' || operator1 === '/') && (operator2 === '+' || operator2 === '-')) return false;
+    checkPrecedence(operator1, operator2) {
+        if (operator2 === '(' || operator2 === ')') return false;
+        if ((operator1 === '*' || operator1 === '/') && (operator2 === '+' || operator2 === '-')) return false;
         return true;
     }
 
     calculate(operand1, operator, operand2) {
-        if (operator === '-') return operand1 - operand2;  
+        if (operator === '-') return operand1 - operand2;
         if (operator === '+') return operand1 + operand2;
         if (operator === '*') return operand1 * operand2;
         else return operand1 / operand2;
@@ -65,7 +63,7 @@ class Calculator {
 
         try {
             for (let i = 0; i < expression.length; i++) {
-
+                
                 if (expression[i] === '(' || expression[i] === ')') {
                     if (this.validateParentheses(expression[i], expression[i + 1])) {
                         array.push(expression[i]);
@@ -74,24 +72,17 @@ class Calculator {
                     }
                 } else if (expression[i] === '*' || expression[i] === '/' || expression[i] === '+') {
 
-                    if (expression[i + 1] === '/' || expression[i + 1] === '*' || expression[i + 1] === '+') {
+                    if (i === 0 || i === expression.length - 1 || expression[i + 1] === '/' || expression[i + 1] === '*' || expression[i + 1] === '+') {
                         throw new Error('Invalid Syntax');
                     }
-
                     array.push(expression[i]);
 
-                } else if (expression[i] === '-') {
-                    
-                    if (expression[i + 1] === '-' && expression[i + 2] === '+' || expression[i + 2] === '-' || expression[i + 2] === '*' || expression[i + 2] === '/'
-                    || expression[i + 2] === '(' || expression[i + 2] === ')' || (i + 1 === expression.length - 1 && i !== 0)) {
-                        throw new Error('Invalid Syntax');
-                    }
-
-                    if (expression[i - 1] !== '+' && expression[i - 1] !== '*' && i > 0 && expression[i - 1] !== '/' && expression[i - 1] !== '-') {
-                        array.push(expression[i]);
-                    }
+                } else if (expression[i] === '-' && expression[i + 1] === '-' && (expression[i + 2] === '+' || expression[i + 2] === '-' || expression[i + 2] === '*' || expression[i + 2] === '/'
+                    || expression[i + 2] === '(' || expression[i + 2] === ')' || i + 1 === expression.length - 1 || i === 0)) {
+                    throw new Error('Invalid Syntax');
+                } else if (expression[i] === '-' && expression[i - 1] !== '+' && expression[i - 1] !== '*' && expression[i - 1] !== '-' && expression[i - 1] !== '/' && i > 0) {
+                    array.push(expression[i], );
                 } else {
-                    
                     const floatNum = Number.parseFloat(expression.slice(i));
                     const num = Number.parseInt(expression.slice(i));
 
@@ -116,12 +107,12 @@ class Calculator {
     }
 
     validateParentheses(parenthesis, nextElement) {
-        if (nextElement === '(' || nextElement === ')' || 
-        (parenthesis === ')' && this.openParentheses === 0)) return false;
-        
+        if (nextElement === '(' || nextElement === ')' ||
+            (parenthesis === ')' && this.openParentheses === 0)) return false;
+
         if (parenthesis === '(') this.openParentheses++;
         else this.openParentheses--;
-        
+
         return true;
     }
 }
